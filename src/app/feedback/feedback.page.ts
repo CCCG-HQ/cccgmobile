0f70f30f107bfe638a04bbb8ed1b1f5879990a89
cccgit-team <85400654+cccgit-team@users.noreply.github.com>
2021-10-16 13:44:25 -0600

    changing issues with the forms

--- src/app/feedback/feedback.page.ts
+++ src/app/feedback/feedback.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Email } from '../../../src/assets/smtp.js';
 import { AlertController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { HTTP } from '@ionic-native/http/ngx';
 
 @Component({
   selector: 'app-feedback',
@@ -14,8 +15,9 @@ export class FeedbackPage implements OnInit {
   last: any;
   phone: any;
   feedture: any;
+  url: any;
 
-  constructor(public alertController: AlertController, public router: Router) { }
+  constructor(public alertController: AlertController, public router: Router, private http: HTTP) { }
 
   async presentAlert() {
     const alert = await this.alertController.create({
@@ -31,31 +33,97 @@ export class FeedbackPage implements OnInit {
     this.router.navigate(['/']);
   }
 
+  async validAlert(message) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Feedback/Feature Request',
+      subHeader: 'Missing info!',
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    //this.router.navigate(['/']);
+  }
+
   ngOnInit() {
   }
 
   submit(){
-    console.log("emailing");
+    //console.log("emailing");
 
-    var body = "First name : " + this.first + '\n' + 
-    "Last name : " + this.last + '\n' + 
-    "Phone : " + this.phone + '\n' + 
-    "Feedback/Feature Request : " + this.feedture + '\n';
+    if(this.first == "" || this.first == null)
+    {
+       this.validAlert("Please enter a first name");
+    }
+
+    if(this.feedture == "" || this.feedture == null)
+    {
+      this.validAlert("Please enter your feedback or feature request")
+    }
+
+    if(this.first != ""  && this.feedture != "" && this.first != null && this.feedture != null){
+      var body = "First name : " + this.first + '<br/><br/>' + 
+      "Last name : " + this.last + '<br/><br/>' + 
+      "Phone : " + this.phone + '<br/><br/>' + 
+      "Feedback/Feature Request : " + this.feedture + '<br/><br/>';
+
+      this.url = 
+
+      "https://api.elasticemail.com/v2/email/send?apikey=AC7071D99A31A0A95279BF4802C6B080A15A5F2E83C71DD90BFB3254B7489DC54EBD545CDD674275EB8E9AA355385BA8&subject=" 
+
+      + "Feedback/Feature Submission from CCCG Mobile!" + 
+
+      "&from=cccgit@gmail.com&fromName=CCCG&sender=cccgit@gmail.com&senderName=CCCG&msgFrom=&msgFromName=&replyTo=&replyToName=&to=cccgit@gmail.com&msgTo=&msgCC=&msgBcc=&lists=&segments=&mergeSourceFilename=&dataSource=&channel=&bodyHtml="
+      
+      + body +
+      
+      "&bodyText=" 
+
+      + body + 
+
+      "&charset=&charsetBodyHtml=&charsetBodyText=&encodingType=4&template=&headers_firstname=firstname: myValueHere&postBack=&merge_firstname=John&timeOffSetMinutes=&poolName=My Custom Pool&isTransactional=false&attachments=&trackOpens=true&trackClicks=true&utmSource=source1&utmMedium=medium1&utmCampaign=campaign1&utmContent=content1&bodyAmp=&charsetBodyAmp=";
+    
+      //console.log(this.url);
+
+      this.sendEmail();
+
+    }
 
 
-    //alert(body);
+  }
+
+  sendEmail(){
+
+    //console.log("sending");
+
+    let headers = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Methods': 'GET'
+    };
 
 
-    Email.send({
-      Host : 'smtp.elasticemail.com',
-      Username : 'cccgit@gmail.com',
-      Password : 'A21F5920919F984EF2E759A1BD3DD986266D',
-      To : 'cccgit@gmail.com',
-      From : 'cccgit@gmail.com',
-      Subject : 'Feedback/Feature submission (CCCG Mobile): ' + this.first,
-      Body : body
-      }).then( this.presentAlert());
+    return new Promise((resolve, reject) => {
 
+     // this.http.setDataSerializer('json');
+
+      this.http.post(this.url, {}, {}).then(res =>{
+        this.presentAlert();
+        resolve(JSON.parse(res.data));
+      })
+      .catch(err =>{
+        alert("Y" + err);
+        reject(err);
+      });
+    });
+
+
+    /* return this.http.get(this.url,headers).pipe(map(data => { this.database=data; console.log(data); })).subscribe(result => {
+      console.log(result);
+    }); */
   }
 
 }
--- src/app/ride/ride.page.ts
+++ src/app/ride/ride.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { Email } from '../../../src/assets/smtp.js';
 import { Router } from '@angular/router';
+import { HTTP } from '@ionic-native/http/ngx';
 
 @Component({
   selector: 'app-ride',
@@ -16,8 +17,9 @@ export class RidePage implements OnInit {
   phone: any;
   address: any;
   service: any;
+  url: any;
 
-  constructor(public alertController: AlertController, public router: Router) { }
+  constructor(public alertController: AlertController, public router: Router, private http: HTTP) { }
 
   async presentAlert() {
     const alert = await this.alertController.create({
@@ -33,33 +35,118 @@ export class RidePage implements OnInit {
     this.router.navigate(['/']);
   }
 
+  async validAlert(message) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'CCCG Ride Request',
+      subHeader: 'Missing info!',
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    //this.router.navigate(['/']);
+  }
+
   ngOnInit() {
   }
 
   submit(){
-    console.log("emailing");
+    //console.log("emailing");
 
-    var body = "First name : " + this.first + '\n' + 
-    "Last name : " + this.last + '\n' + 
-    "Email: " + this.email + '\n' + 
-    "Phone : " + this.phone + '\n' + 
-    "Address : " + this.address + '\n' +
-    "Service : " + this.service + '\n';
+    if(this.first == "" || this.first == null)
+    {
+       this.validAlert("Please enter a first name");
+    }
+
+    if(this.email == "" || this.email == null)
+    {
+      this.validAlert("Please enter your email")
+    }
+
+    if(this.phone == "" || this.phone == null)
+    {
+      this.validAlert("Please enter a phone number we can use to reach you or N/A")
+    }
+
+    if(this.address == "" || this.address == null)
+    {
+      this.validAlert("Please enter your address")
+    }
+
+    if(this.service == "" || this.service == null)
+    {
+      this.validAlert("Please enter indicate what service/program you'd like to attend")
+    }
+
+    if(this.first != "" && this.email != "" &&  this.phone != "" && this.address != "" && this.service != ""
+    && this.first != null && this.email != null &&  this.phone != null && this.address != null && this.service != null )
+    {
+        var body = "First name : " + this.first + '<br/><br/>' + 
+        "Last name : " + this.last + '<br/><br/>' + 
+        "Email: " + this.email + '<br/><br/>' + 
+        "Phone : " + this.phone + '<br/><br/>' + 
+        "Address : " + this.address + '<br/><br/>' +
+        "Service : " + this.service + '<br/><br/>';
 
 
-    //alert(body);
+        this.url = 
+
+        "https://api.elasticemail.com/v2/email/send?apikey=AC7071D99A31A0A95279BF4802C6B080A15A5F2E83C71DD90BFB3254B7489DC54EBD545CDD674275EB8E9AA355385BA8&subject=" 
+
+        + "Ride Request Submission from CCCG Mobile!" + 
+
+        "&from=cccgit@gmail.com&fromName=CCCG&sender=cccgit@gmail.com&senderName=CCCG&msgFrom=&msgFromName=&replyTo=&replyToName=&to=cccgit@gmail.com&msgTo=&msgCC=&msgBcc=&lists=&segments=&mergeSourceFilename=&dataSource=&channel=&bodyHtml="
+        
+        + body +
+        
+        "&bodyText=" 
+
+        + body + 
+
+        "&charset=&charsetBodyHtml=&charsetBodyText=&encodingType=4&template=&headers_firstname=firstname: myValueHere&postBack=&merge_firstname=John&timeOffSetMinutes=&poolName=My Custom Pool&isTransactional=false&attachments=&trackOpens=true&trackClicks=true&utmSource=source1&utmMedium=medium1&utmCampaign=campaign1&utmContent=content1&bodyAmp=&charsetBodyAmp=";
+      
+        //console.log(this.url);
+
+        this.sendEmail();
 
 
-    Email.send({
-      Host : 'smtp.elasticemail.com',
-      Username : 'cccgit@gmail.com',
-      Password : 'A21F5920919F984EF2E759A1BD3DD986266D',
-      To : 'cccgit@gmail.com',
-      From : 'cccgit@gmail.com',
-      Subject : 'CCCG Ride Request Submission (CCCG Mobile): ' + this.first,
-      Body : body
-      }).then( this.presentAlert());
+    }
+    
 
+  }
+
+  sendEmail(){
+
+    //console.log("sending");
+
+    let headers = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Methods': 'GET'
+    };
+
+
+    return new Promise((resolve, reject) => {
+
+     // this.http.setDataSerializer('json');
+
+      this.http.post(this.url, {}, {}).then(res =>{
+        this.presentAlert();
+        resolve(JSON.parse(res.data));
+      })
+      .catch(err =>{
+        alert("Y" + err);
+        reject(err);
+      });
+    });
+
+
+    /* return this.http.get(this.url,headers).pipe(map(data => { this.database=data; console.log(data); })).subscribe(result => {
+      console.log(result);
+    }); */
   }
 
 }
